11530199aa5752596b0aaaf047eea18f2533d956
Paul Houlston <paul.houlston@hotmail.co.uk>
2025-01-18 09:30:25 +0000

    Refact test into GWT

--- tests/PricingEngine.Tests/EmptyPricingEngineTests.cs
+++ tests/PricingEngine.Tests/EmptyPricingEngineTests.cs
@@ -2,13 +2,30 @@
 
 public class EmptyPricingEngineTests
 {
+    private PricingRulesExecutor _pricingExecutor;
+    private int _price;
+    
     [Fact]
     public void Test_that_I_get_default_zero_price_when_no_rules_are_added()
     {
-        var price = 0;
-        var pricingExecutor = new PricingRulesExecutor();
-        price = pricingExecutor.GetPrice();
-        Assert.Equal(0, price);
+        Given_no_rules_are_added();
+        When_I_execute_the_pricing_rules();
+        Then_the_price_is_zero();
+    }
+
+    private void Given_no_rules_are_added()
+    {
+        _pricingExecutor = new PricingRulesExecutor();
+    }
+
+    private void When_I_execute_the_pricing_rules()
+    {
+        _price = _pricingExecutor.GetPrice();
+    }
+
+    private void Then_the_price_is_zero()
+    {
+        Assert.Equal(0, _price);
     }
 }
 
