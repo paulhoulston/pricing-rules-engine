152c18ca63292aa4f477138da075399cd23281a1
Paul Houlston <paul.houlston@hotmail.co.uk>
2025-01-18 09:25:51 +0000

    Added first test to check price is zero if no rules are defined

---
+++ tests/PricingEngine.Tests/EmptyPricingEngineTests.cs
@@ -0,0 +1,21 @@
+﻿namespace PricingEngine.Tests;
+
+public class EmptyPricingEngineTests
+{
+    [Fact]
+    public void Test_that_I_get_default_zero_price_when_no_rules_are_added()
+    {
+        var price = 0;
+        var pricingExecutor = new PricingRulesExecutor();
+        price = pricingExecutor.GetPrice();
+        Assert.Equal(0, price);
+    }
+}
+
+public class PricingRulesExecutor
+{
+    public int GetPrice()
+    {
+        return 0;
+    }
+}
