ae2eda74360a4bcc85b771ffa60f5b60906c6aad
Paul Houlston <paul.houlston@hotmail.co.uk>
2025-01-20 18:54:40 +0000

    Added currency to output

--- tests/PricingEngine.Tests/EmptyPricingEngineTests.cs
+++ tests/PricingEngine.Tests/EmptyPricingEngineTests.cs
@@ -3,18 +3,21 @@
 public class EmptyPricingEngineTests
 {
     private PricingRulesExecutor _pricingExecutor;
-    private int _price;
-    
+
+    private PricingRulesExecutor.Price _price;
+
     [Fact]
     public void Test_that_I_get_default_zero_price_when_no_rules_are_added()
     {
-        Given_no_rules_are_added();
+        Given_that_I_initialize_the_currency_to_GBP_and_assign_no_rules();
         When_I_execute_the_pricing_rules();
         Then_the_price_is_zero();
+        Then_the_currency_is_GBP();
     }
 
-    private void Given_no_rules_are_added()
+    private void Given_that_I_initialize_the_currency_to_GBP_and_assign_no_rules()
     {
+        // Initialize the currency to GBP
         _pricingExecutor = new PricingRulesExecutor();
     }
 
@@ -25,14 +28,25 @@ public class EmptyPricingEngineTests
 
     private void Then_the_price_is_zero()
     {
-        Assert.Equal(0, _price);
+        Assert.Equal(0, _price.Amount);
+    }
+
+    private void Then_the_currency_is_GBP()
+    {
+        Assert.Equal("GBP", _price.Currency);
     }
 }
 
 public class PricingRulesExecutor
 {
-    public int GetPrice()
+    public class Price
     {
-        return 0;
+        public int Amount { get; set; }
+        public string Currency { get; set; }
+    }
+ 
+    public Price GetPrice()
+    {
+        return new Price { Amount = 0, Currency = "GBP" };
     }
 }
